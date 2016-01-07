f481b10948eadfcd6631fe9460af943b4f11a8e7
w <your@email.com>
2016-01-07 18:35:18 -0500

    AssaultCube updated with silent install flag

--- Casks/assault-cube.rb
+++ Casks/assault-cube.rb
@@ -1,11 +1,12 @@
 cask :v1 => 'assault-cube' do
   version '1.2.0.2'
-  sha256 'ee69217202a66a518a688980e55ab2bd278fe46ad47461cf2a6eeef353d5e9e9'
-  url 'https://www.mirrorservice.org/sites/downloads.sourceforge.net/p/po/portableapps/Assault%20Cube%20Portable/AssaultCubePortable_1.2.0.2_English.paf.exe'
+  sha256 'f048f6f046b4954dfd9c3e41a6b48b94167f8f93847dbf2a7820fdef4b85ea7e'
+  url 'https://github.com/assaultcube/AC/releases/download/v1.2.0.2/AssaultCube_v1.2.0.2.exe'
   name 'Assault Cube'
   homepage 'http://assault.cubers.net/'
 
-  win_exe 'AssaultCubePortable_1.2.0.2_English.paf.exe'
+  win_exe 'AssaultCube_v1.2.0.2.exe'
+  wine_install_flags '/S'
 
   depends_on []
   wine_version '1.6'
