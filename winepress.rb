779b9b3a43abad495a317fa598085c262a46f50f
Garland <garland220@yahoo.com>
2016-01-07 18:02:05 -0500

    Setting up tap

---
+++ winepress.rb
@@ -0,0 +1,18 @@
+class Winepress < Formula
+  homepage "https://github.com/winepress/winepress/"
+  url "https://github.com/winepress/winepress.git", :tag => "v0.0.1"
+
+  depends_on :ruby => "2.0"
+
+  def install
+  end
+
+  def caveats
+    <<-EOS.undent
+    EOS
+  end
+
+  test do
+  end
+end
+
