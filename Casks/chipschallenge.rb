f6791a8edc4e9e93e628706c724010c448644f10
w <your@email.com>
2016-01-08 17:40:05 -0500

    chipschallenge: fixed name

--- Casks/chipschallenge.rb
+++ Casks/chipschallenge.rb
@@ -3,7 +3,7 @@ cask :v1 => 'chipschallenge' do
   sha256 '8735a48236ed823d687186db98fbc3a541fe765ae792a9fb912ec9899e369298'
   url 'http://www.scuzzstuff.org/chipschallenge/cclp2.zip'
 
-  name 'Chip's Challenge'
+  name 'Chip\'s Challenge'
   homepage 'https://en.wikipedia.org/wiki/Chip%27s_Challenge'
 
   win_exe 'CHIPS.EXE'
