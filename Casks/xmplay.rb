341fb1facfc6fc9f038591059db281ecee2e1796
w <your@email.com>
2016-01-08 18:05:35 -0500

    xmplay: make it actually work

--- Casks/xmplay.rb
+++ Casks/xmplay.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'xmplay' do
   version '3.8.2'
   sha256 'a6a2f4431cf3e637374bad680774fc452de5e1c74a379732d59789673f53a952'
-  url 'http://www.un4seen.com/download.php?xmplay38'
+  url 'http://www.un4seen.com/files/xmplay38.zip'
 
   name 'XMPlay'
-  homepage 'http://www.un4seen.com/'
+  homepage 'http://www.un4seen.com/xmplay.html'
 
   win_exe 'xmplay.exe'
 
