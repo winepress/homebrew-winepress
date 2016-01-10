90ee2a73eb7041137247cca5c5b3f17f4925bcf5
Garland <garland220@yahoo.com>
2016-01-10 11:02:54 -0500

    Additional configuration for MWO

--- Grapes/mechwarrior-online.rb
+++ Grapes/mechwarrior-online.rb
@@ -11,7 +11,8 @@ cask :v1 => 'mechwarrior-online' do
   depends_on []
   wine_version '1.8'
   wine_arch 'win32'
-  wine_tricks ['corefonts', 'd3dx9_43', 'msxml3', 'dotnet40', 'vcrun2010']
+  wine_tricks ['winxp', 'corefonts', 'd3dx9_43', 'msxml3', 'dotnet40', 'vcrun2010', 'ddr=opengl']
+  # override_dlls ['atl100', 'd3dx9_43', 'mscoree', 'msvcp100', 'msvcr100', 'msxml3']
 end
 
 
