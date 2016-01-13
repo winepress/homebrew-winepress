b1d51c6683160b63a3568006886a6ce661c07cd2
w <your@email.com>
2016-01-13 17:17:21 -0500

    eve-online: mirror added

--- Grapes/eve-online.rb
+++ Grapes/eve-online.rb
@@ -2,6 +2,7 @@ cask :v1 => 'eve-online' do
   version '1'
   sha256 'b297a2136b14ddbe4fea824158e81f2cd8c9fbedc7dabf48dc7ef691c7b99528'
   url 'https://web.archive.org/web/20160113220656/http://binaries.eveonline.com/EveLauncher-995234.exe'
+  mirror 'http://binaries.eveonline.com/EveLauncher-995234.exe'
   name 'EVE Online'
   homepage 'https://www.eveonline.com/'
 
