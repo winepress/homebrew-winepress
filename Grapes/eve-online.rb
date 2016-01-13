01ad1b620740cff67aa64ce28f6c82f1e5b26657
w <your@email.com>
2016-01-13 17:12:58 -0500

    eve-online: added archive.org link for installer

--- Grapes/eve-online.rb
+++ Grapes/eve-online.rb
@@ -1,13 +1,12 @@
 cask :v1 => 'eve-online' do
   version '1'
-  sha256 'ee69217202a66a518a688980e55ab2bd278fe46ad47461cf2a6eeef353d5e9e9'
-  url 'http://binaries.eveonline.com/EveLauncher-995234.exe'
+  sha256 'b297a2136b14ddbe4fea824158e81f2cd8c9fbedc7dabf48dc7ef691c7b99528'
+  url 'https://web.archive.org/web/20160113220656/http://binaries.eveonline.com/EveLauncher-995234.exe'
   name 'EVE Online'
   homepage 'https://www.eveonline.com/'
 
   win_exe 'ExeFile.exe'
 
-  depends_on []
   wine_version '1.8'
   wine_arch 'win32'
   wine_tricks ['corefonts', 'd3dx9_36', 'd3dx9_42', 'vcrun2005', 'vcrun2008', 'vcrun2010', 'gecko']
