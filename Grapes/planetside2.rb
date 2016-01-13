29e496fcd8eefff36f629ea99e6d6eb56b728336
w <your@email.com>
2016-01-13 17:15:44 -0500

    planetside2: added archive.org installer

--- Grapes/planetside2.rb
+++ Grapes/planetside2.rb
@@ -1,14 +1,13 @@
 cask :v1 => 'planetside2' do
   version '1'
-  sha256 ''
-  url 'https://launch.daybreakgames.com/installer/PS2_setup.exe'
+  sha256 '95464498ead3a9e47c5b3f7f46a1a170a2fbfd854f472c6d9c24c66e8dffa13e'
+  url 'https://web.archive.org/web/20150909054550/https://launch.daybreakgames.com/installer/PS2_setup.exe'
   name 'Planetside 2'
   homepage 'https://www.planetside2.com'
 
   win_exe ''
   install_exe 'PS2_setup.exe'
 
-  depends_on []
   wine_version '1.8'
   wine_arch 'win32'
   wine_tricks ['corefonts', 'd3dx9_43', 'd3dcompiler_43', 'vcrun2008', 'vcrun2010']
